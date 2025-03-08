956c32dcdaca9b4482af994de93dba52843c701c
annapirz <fanworld991@gmail.com>
2025-03-08 21:53:11 +0300

    feat: add form

--- src/pages/RegistrationForm.jsx
+++ src/pages/RegistrationForm.jsx
@@ -4,6 +4,15 @@ const RegistrationForm = () => {
     return  (
         <>
             <p>тут пользователь будет регистрировать свой профиль</p>
+            <form>
+                <input type="text" placeholder="введите ваше имя"/>
+                <input type="text" placeholder='введите город в котором вы хотите сходить на чашечку кофе'/>
+                <input type="text" placeholder='введите вашу профессию'/>
+                <input type="text" placeholder='введите стаж работы'/>
+                <input type="text" placeholder='расскажите немного о себе'/>
+
+                <button onSubmit='submmit'>отправить данные</button>
+            </form>
         </>
     )
 }
